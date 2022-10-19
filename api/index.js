3a2d6a1b05f849cee1a20167a062e7c1717a5737
moomdate <moomdate@gmail.com>
2022-10-19 14:55:34 +0700

    add rainbow animate

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 export default function handler(request, response) {
-  response.setHeader("Content-Type", "image/svg+xml");
+    response.setHeader("Content-Type", "image/svg+xml");
 
-  const svgString = `
-  <svg width="500" height="250" xmlns="http://www.w3.org/2000/svg">
-    <style>
-      @keyframes fadeIn {
+    const svgString = `
+<svg width="500" fill="" height="250" xmlns="http://www.w3.org/2000/svg">
+  <style>
+    @keyframes fadeIn {
           from, to { opacity: 0 }
           50% { opacity: 1; }
       }
@@ -13,14 +13,28 @@ export default function handler(request, response) {
         animation-iteration-count: infinite;
       }
       .text {
-        font-family: 'Helvetica', sans-serif;
+        font-family: &apos;Helvetica&apos;, sans-serif;
       }
-    </style>
-    <rect width="100%" height="100%" fill="white"></rect>
-    <text x="47%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="6rem" class="text">ODDS</text>
-    <line x1="77%" x2="77%" y1="15%" y2="70%" stroke="black" stroke-width="10" class="indicator"/>
-  </svg>
+  </style>
+  <defs>
+    <linearGradient id="gradient" gradientTransform="rotate(0)">
+      <stop offset="0" stop-color="red">
+        <animate attributeName="stop-color" values="red;green;blue;pink;yellow;orange;red;purple" dur="10s" repeatCount="indefinite"/>
+      </stop>
+      <stop offset="1" stop-color="purple" stop-opacity="purple">
+        <animate attributeName="stop-color" values="purple;blue;green;yellow;orange;red;purple;" dur="10s" repeatCount="indefinite"/>
+      </stop>
+      <stop offset="2" stop-color="red" stop-opacity="0">
+        <animate attributeName="stop-color" values="red;orange;yellow;green;blue;indigo;violet;" dur="10s" repeatCount="indefinite"/>
+      </stop>
+    </linearGradient>
+  </defs>
+  <rect width="100%" height="100%" fill="white"/>
+  <text x="47%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="url(#gradient)" font-size="6rem" class="text">ODDS</text>
+  <rect x="77%" y="16%" width="11" height="55%" class="indicator" fill="url(#gradient)"/>
+</svg>
+
   `;
 
-  return response.send(svgString);
+    return response.send(svgString);
 }
